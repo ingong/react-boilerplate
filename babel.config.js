886893fd4d10d08a47ebf6631e18b8074ed9081f
ingong <insong1216@gmail.com>
2021-10-13 23:48:50 +0900

    fix: regeneratorRunTime is not defined 문제 해결

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
 
   const presets = ['@babel/preset-env', '@babel/preset-react'];
 
-  const plugins = [];
+  const plugins = ['@babel/plugin-transform-runtime'];
 
   return {
     presets,
